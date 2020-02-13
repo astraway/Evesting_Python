e79dbd61646618d41b4ce61d2ab2c5142a423bc9
Eric Jurotich <ejurotic@cisco.com>
2020-02-12 16:12:12 -0800

    Implemented Factory for processors using ABC

--- Main.py
+++ Main.py
@@ -1,103 +1,20 @@
+from fastapi import FastAPI
+from Manual_Main import Evesting
 import pandas as pd
-import logging
-import os
-from logging.handlers import TimedRotatingFileHandler
-import settings
-from  SqlConnection import Sql
-from Processors.ProcessorStockPrice import StockPrice
-from Processors.ProcessorNetIncome import NetIncome
-from Processors.ProcessorOperatingCash import OperatingCash
-from processorfactory import ProcessorFactory
 
-__app_name__ = 'EVesting_Main'
-
-# root logger
-root_logger = logging.getLogger()
-
-root_logger.setLevel(logging.INFO)
-
-# stream handler
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-root_logger.addHandler(stream_handler)
-
-# file handler
-if not os.path.exists(settings.DIRECTORY_LOGS):
-    os.makedirs(settings.DIRECTORY_LOGS)
-logfile = '{}/{}.log'.format(settings.DIRECTORY_LOGS, __app_name__)
-fileHandler = TimedRotatingFileHandler(logfile, when='midnight', backupCount=7)
-fileHandler.setFormatter(formatter)
-root_logger.addHandler(fileHandler)
-
-# local logger
-logger = logging.getLogger(__app_name__)
+app = FastAPI()
 
 
-class Evesting:
-
-    def __init__(self):
-
-
-        column_names = ["STOCK_TICKER", "NET_INCOME", "OPERATING_CASH"]
-        co_value_investing_data = pd.DataFrame(columns = column_names)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
+@app.get("/items/{stock_ticker}")
+def read_item(stock_ticker: str):
 
-        sqlite_connection_dict = {
+    evst = Evesting(stock_ticker)
+    json_obj = evst.process_stock()
 
-            'sqlite_co_value_investing_data_table' : "value_investing",
-            'sqlite_Financials_table': "financials",
-            'sqlite_Net_Income_table': "net_income",
-            'sqlite_Operating_Cash_table': 'operating_cash'
-
-        }
-
-        factory = ProcessorFactory()
-
-        #sqal_engine = Sql.create_postgres_engine(self)
-        sqal_engine = Sql.create_sqlite_engine(self)
-        query = '''select * from financials'''
-        fin_data = Sql.sqlite_read(self, query, sqal_engine, sqlite_connection_dict)
-        print(fin_data.head())
-
-
-
-
-        print("What stock ticker would you like to know about ? : ")
-
-
-        #takes user input for stock tick...
-        stock_ticker = input()
-        co_value_investing_data = co_value_investing_data.append({'STOCK_TICKER': stock_ticker}, ignore_index=True)
-
-
-        logger.info('Retreiving Stock Price...')
-        sp= factory.create_instance("StockPrice")
-        sp.processor(stock_ticker,co_value_investing_data)
-
-
-
-
-
-        logger.info('Retreiving Net Income...')
-        ni = factory.create_instance("NetIncome")
-        co_value_investing_data = ni.processor(stock_ticker, co_value_investing_data)
-
-
-
-
-        logger.info('Retreiving Operating Cash...')
-        oc = factory.create_instance("OperatingCash")
-        co_value_investing_data = oc.processor(stock_ticker, co_value_investing_data)
-
-
-
-        #writting all values to SQL
-        logger.info('Writing co_value_investing_data to sqlite...')
-        Sql.sqlite_df_insert(self, sqal_engine, sqlite_connection_dict['sqlite_co_value_investing_data_table'], co_value_investing_data)
-
-
-        print(co_value_investing_data.head())
-
-Evesting()
+    #  to access locally http: // localhost: 8000 / items /
+    return json_obj
---
+++ Manual_Main.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import logging
+import os
+from logging.handlers import TimedRotatingFileHandler
+import settings
+from  SqlConnection import Sql
+from Processors.ProcessorStockPrice import StockPrice
+from Processors.ProcessorNetIncome import NetIncome
+from Processors.ProcessorOperatingCash import OperatingCash
+from processorfactory import ProcessorFactory
+import json
+__app_name__ = 'EVesting_Main'
+
+# root logger
+root_logger = logging.getLogger()
+
+root_logger.setLevel(logging.INFO)
+
+# stream handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+root_logger.addHandler(stream_handler)
+
+# file handler
+if not os.path.exists(settings.DIRECTORY_LOGS):
+    os.makedirs(settings.DIRECTORY_LOGS)
+logfile = '{}/{}.log'.format(settings.DIRECTORY_LOGS, __app_name__)
+fileHandler = TimedRotatingFileHandler(logfile, when='midnight', backupCount=7)
+fileHandler.setFormatter(formatter)
+root_logger.addHandler(fileHandler)
+
+# local logger
+logger = logging.getLogger(__app_name__)
+
+
+class Evesting:
+
+    def __init__(self,stock_ticker):
+        self.stock_ticker = stock_ticker
+        self.process_stock()
+
+    def process_stock(self):
+        column_names = ["STOCK_TICKER", "NET_INCOME", "OPERATING_CASH"]
+        co_value_investing_data = pd.DataFrame(columns = column_names)
+
+
+
+        sqlite_connection_dict = {
+
+            'sqlite_co_value_investing_data_table' : "value_investing",
+            'sqlite_Financials_table': "financials",
+            'sqlite_Net_Income_table': "net_income",
+            'sqlite_Operating_Cash_table': 'operating_cash'
+
+        }
+
+        factory = ProcessorFactory()
+
+        #sqal_engine = Sql.create_postgres_engine(self)
+        sqal_engine = Sql.create_sqlite_engine(self)
+        query = '''select * from financials'''
+        fin_data = Sql.sqlite_read(self, query, sqal_engine, sqlite_connection_dict)
+        print(fin_data.head())
+
+
+
+
+        #print("What stock ticker would you like to know about ? : ")
+
+
+        #takes user input for stock tick...
+        stock_ticker = self.stock_ticker
+        co_value_investing_data = co_value_investing_data.append({'STOCK_TICKER': stock_ticker}, ignore_index=True)
+
+
+        logger.info('Retreiving Stock Price...')
+        sp= factory.create_instance("StockPrice")
+        sp.processor(stock_ticker,co_value_investing_data)
+
+
+
+
+
+        logger.info('Retreiving Net Income...')
+        ni = factory.create_instance("NetIncome")
+        co_value_investing_data = ni.processor(stock_ticker, co_value_investing_data)
+
+
+
+
+        logger.info('Retreiving Operating Cash...')
+        oc = factory.create_instance("OperatingCash")
+        co_value_investing_data = oc.processor(stock_ticker, co_value_investing_data)
+
+
+
+        #writting all values to SQL
+        logger.info('Writing co_value_investing_data to sqlite...')
+        Sql.sqlite_df_insert(self, sqal_engine, sqlite_connection_dict['sqlite_co_value_investing_data_table'], co_value_investing_data)
+        #convert df to json string
+        str_json =  co_value_investing_data.head().to_json(orient='index')
+        #convert string to a json object
+        json_obj = json.loads(str_json)
+
+
+        return json_obj['0']
+
+#Evesting()
